ffa37b98f50585ca54d4c952dc2dffd4095d1555
AlexanderPaul1983 <alexanderpaul@gmx.de>
2024-10-18 09:08:18 +0200

    fixed links for AGB and Datenschutz

--- pages/kontakt/kontakt.js
+++ pages/kontakt/kontakt.js
@@ -2,27 +2,25 @@
 
 
 function sendMail() {
-    // Zuerst die serviceId und templateId holen
+
     fetch("/kontakt")
-        .then(response => response.json()) 
+        .then(response => response.json())
         .then(data => {
             const serviceId = data.serviceId;
             const templateId = data.templateId;
-            console.log(serviceId + " " + templateId);
-            
-            // Jetzt erst die Nachricht senden, nachdem die IDs geladen wurden
+
             var params = {
                 vorname: document.getElementById("name").value,
                 email: document.getElementById("email").value,
                 message: document.getElementById("message").value
             };
-            
+
             return emailjs.send(serviceId, templateId, params);
         })
-        .then(function(res) {
+        .then(function (res) {
             alert("Gesendet");
         })
-        .catch(function(error) {
+        .catch(function (error) {
             console.error("Fehler beim Senden der Nachricht:", error);
             alert("Fehler beim Senden der Nachricht.");
         });
