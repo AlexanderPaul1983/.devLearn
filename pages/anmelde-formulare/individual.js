fac603147a809a3bcc5fca6abead0c168841e1cb
AlexanderPaul1983 <alexanderpaul@gmx.de>
2024-10-18 09:43:56 +0200

    send form was created in individual

--- pages/anmelde-formulare/individual.js
+++ pages/anmelde-formulare/individual.js
@@ -1,40 +1,33 @@
 
-let serviceId;
-let templateId;
 
-fetch("/anmelde-formulare")
-    .then(response => response.json())
-    .then(data => {
-        serviceId = data.serviceId_individual;
-        templateId = data.templateId_individual;
-    })
-    .catch(error => {
-        console.error('Error fetching serviceId and templateId:', error);
-    });
 
 function sendAnfrage() {
-    let params =
-    {
-        vorname: document.getElementById('vorname').value,
-        nachname: document.getElementById('nachname').value,
-        alter: document.getElementById('alter').value,
-        name_des_erziehungsberechtigten: document.getElementById('name_des_erziehungsberechtigten').value,
-        email: document.getElementById('email').value,
-        telefon: document.getElementById('telefon').value,
-    };
 
+    fetch("/anmelde-formulare")
+        .then(response => response.json())
+        .then(data => {
+            const serviceId = data.serviceId_individual;
+            const templateId = data.templateId_individual;
 
+            let params =
+            {
+                vorname: document.getElementById('vorname').value,
+                nachname: document.getElementById('nachname').value,
+                alter: document.getElementById('alter').value,
+                name_des_erziehungsberechtigten: document.getElementById('name_des_erziehungsberechtigten').value,
+                email: document.getElementById('email').value,
+                telefon: document.getElementById('telefon').value,
+            };
 
-
-    emailjs.send(serviceId, templateId, params)
+            return emailjs.send(serviceId, templateId, params);
+        })
         .then(function (res) {
             alert("Gesendet");
         })
         .catch(function (error) {
-            console.error("Error sending email:", error);
+            console.error("Fehler beim Senden der Nachricht:", error);
             alert("Fehler beim Senden der Nachricht.");
         });
-
 }
 
 
